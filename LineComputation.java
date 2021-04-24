711d588160777e7d9ad7afbd23fd1466c2de4340
kalpesh60 <kalpeshdhalpe60@gmail.com>
2021-04-24 21:45:19 +0530

    commit-calculated the length of line

--- LineComputation.java
+++ LineComputation.java
@@ -3,4 +3,10 @@ class LineComputation
 public static void main(String[] args)
 {
 System.out.println("Welcome to the Comparison Computation Program");
+int x1=2;
+int x2=3;
+int y1=2;
+int y2=4;
+double LengthOfLine = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+System.out.println("Length of Line:"+"("+x1+","+y1+"),"+"("+x2+","+y2+")"+"is:"+LengthOfLine);
 }}
