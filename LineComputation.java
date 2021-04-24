d0a8d4dffb2ccb1e10a9d61f69a53175c888c0ee
kalpesh60 <kalpeshdhalpe60@gmail.com>
2021-04-24 23:03:31 +0530

    commit-calculated the equality of two lines

--- LineComputation.java
+++ LineComputation.java
@@ -1,5 +1,14 @@
-class LineComputation
+public class LineComputation
 {
+static boolean equals(double LengthOfLine,double LengthOfSecondLine)
+{
+if(LengthOfLine == LengthOfSecondLine)
+{
+return true;
+}
+return false;
+}
+
 public static void main(String[] args)
 {
 System.out.println("Welcome to the Comparison Computation Program");
@@ -7,6 +16,25 @@ int x1=2;
 int x2=3;
 int y1=2;
 int y2=4;
-double LengthOfLine = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+int xaxis=(x2-x1)*(x2-x1);
+int yaxis=(y2-y1)*(y2-y1);
+double LengthOfLine = Math.sqrt((xaxis) + (yaxis));
 System.out.println("Length of Line:"+"("+x1+","+y1+"),"+"("+x2+","+y2+")"+"is:"+LengthOfLine);
+System.out.println("calculating the length of second line");
+int x3=2;
+int x4=3;
+int y3=2;
+int y4=5;
+int xaxis1=(x4-x3)*(x4-x3);
+int yaxis1=(y4-y3)*(y4-y3);
+double LengthOfSecondLine = Math.sqrt((xaxis1) + (yaxis1));
+System.out.println("Length of second Line:"+"("+x3+","+y3+"),"+"("+x4+","+y4+")"+"is:"+LengthOfSecondLine);
+
+boolean Equality = equals(LengthOfLine,LengthOfSecondLine);
+if(Equality)
+System.out.println("both lines are equal");
+else
+{
+System.out.println("both lines are not equal");
+}
 }}
